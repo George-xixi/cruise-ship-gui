3e3c416c3207afb39fc75376355e9acb30ea0479
George Aldington <georgealdington@gmail.com>
2022-11-02 13:47:27 +0000

    added form to add new ports

---
+++ src/form.js
@@ -0,0 +1,17 @@
+
+
+// const portName = document.querySelector("#portName");
+// const form = document.querySelector("#form");
+
+
+// form.addEventListener("submit", (event) => {
+
+//   const newPort = new Port(portName.value);
+//   console.log(newPort);
+//   itinerary.ports.push(newPort);
+//   console.log(itinerary);
+
+//   controller.
+
+//   event.preventDefault();
+// } );
